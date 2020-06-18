873b4aa190095c1e62b7ddfde4e797fb23c70fd0
Nikhil <nlkolluri@gmail.com>
2020-06-18 12:05:04 -0500

    indicator stripe added

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -12,13 +12,17 @@ class CustomNavbar extends Component{
   
     render(){
       return (
+        <div>
         <Navbar className="navBlue">
               <Navbar.Brand><img src="https://cdn.visa.com/cdn/assets/images/logos/visa/logo.png" className="visaLogo"></img></Navbar.Brand>
-              <Nav className="navBlue">
-                <Nav.Link href="#consumer" className="navText" onClick={() =>this.props.action("consumer")}>Consumer</Nav.Link>
-                <Nav.Link href="#merchant" className="navText" onClick={() =>this.props.action("merchant")}>Merchant</Nav.Link>
+              <Nav className="navBlue" id="navOptions">
+                <Nav.Link  className="navText" onClick={() =>this.props.action("consumer")}>Consumer</Nav.Link>
+                <Nav.Link  className="navText" onClick={() =>this.props.action("merchant")}>Merchant</Nav.Link>
               </Nav>
             </Navbar>
+        <div id="indicatorStripe"></div>
+        </div>
+
       )
     }
   }
