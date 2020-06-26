bb23580b4e686c4df7271c96107f2c1c61dc0e56
Eric Wang <ericrwang2011@gmail.com>
2020-06-26 16:50:49 -0500

    updated login

---
+++ src/components/MerchantInvoice.js
@@ -0,0 +1,102 @@
+import React, { Component} from 'react';
+import {Form, Row, Col, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class MerchantInvoice extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+        BusinessName: "",
+        InvoiceAmt: "",
+        InvoiceDesc: "",
+      }
+      this.handleBusinessNameChange = this.handleBusinessNameChange.bind(this)
+      this.handleInvoiceAmtChange = this.handleInvoiceAmtChange.bind(this)
+      this.handleInvoiceDescChange = this.handleInvoiceDescChange.bind(this)
+  }
+
+  handleBusinessNameChange(e){
+    this.setState({BusinessName: e.target.value});
+  }
+
+  handleInvoiceAmtChange(e) {
+    this.setState({InvoiceAmt: e.target.value});
+  }
+
+  handleInvoiceDescChange(e) {
+    this.setState({InvoiceDesc: e.target.value});
+  }
+
+
+  handleSubmit(e){
+    var that=this
+
+    var url = 'https://kylepence.dev:5000/invoices'
+    var myPostBody = {
+        "name": this.state.Name,
+        "businessName": this.state.BusinessName,
+        "country":"USA",
+        "state": this.state.StateInUSA,
+        "zipcode": this.state.ZipCode,
+        "PAN":this.state.PAN,
+        "items": [
+            {
+                "desc": this.state.InvoiceDesc,
+                "amount": this.state.InvoiceAmt
+            }
+        ]
+    }
+
+    fetch(url, {
+      method:"POST",
+      body: JSON.stringify(myPostBody)
+          
+      })
+      .then(result => {
+          console.log(result)
+          // do something with the result
+          result.json().then(data => {
+            console.log(data)
+            console.log(data.result.invoiceCode)
+            that.setState({Alias: data.result.invoiceCode})
+          })
+      })
+    
+    e.preventDefault()
+  }
+
+  render() {
+    
+    const that = this;
+    // Do this for the form submission https://stackoverflow.com/questions/23427384/get-form-data-in-reactjs
+    
+    
+    return (
+        <div className="MerchantInvoice">
+          <Form onSubmit={that.handleSubmit}>
+            <div id="invoiceRow">
+                <Form.Control placeholder="Business name" id="center" onChange={that.handleBusinessNameChange}/>         
+            </div>
+            <div id="invoiceRow">
+                <Form.Control placeholder="Invoice amount" id="center" onChange={that.handleInvoiceAmtChange}/>         
+            </div>
+            <dvi id="invoiceRow">
+                <textarea class="form-control" placeholder="Invoice description" onChange={that.handleInvoiceDescChange} rows="5"></textarea>
+            </dvi>
+            
+            <div className="buttonsRow">
+                <Button variant="primary" type="submit" id="buttonBlue">
+                    <a id='submitText'>Submit</a><a  id='goldenArrow'> {'➤'} </a>
+                </Button>
+            </div>
+          
+          
+          </Form>
+        </div>  
+        
+      );
+  }
+}
+
+export default MerchantInvoice;
--- src/components/MerchantLogin.js
+++ src/components/MerchantLogin.js
@@ -1,31 +1,158 @@
 import React, { Component} from 'react';
 import {Form, Row, Col, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import FirstTimeMerchantForm from './FirstTimeMerchantForm';
+import MerchantInvoice from './MerchantInvoice';
+
 
 
 class MerchantLogin extends Component {
   constructor(props) {
     super(props)
     this.state={
-        Response: "Not yet requested",
+        SignedIn: false,
+        IdToken: null,
+        FormInfoExists: false,
         Alias: "No alias generated",
         SpecialCode: "",
         MessageBox: ""
       }
 
+      this.submitMerchantPayment = this.submitMerchantPayment.bind(this);
   }
+
+  componentDidMount(){
+    var that=this;
+    firebase.auth().onAuthStateChanged((user)=>{
+      if (user !=null) {this.setState({SignedIn: true})}
+      if (this.state.SignedIn){
+        
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+          // Send token to your backend via HTTPS
+          // ...
+          console.log(idToken)
+
+          var url = 'https://kylepence.dev:5000/merchants'
+
+          fetch(url, {
+            method:"GET",
+            headers: {'Authorization': idToken}
+          })
+            .then(result => {
+                console.log(result)
+                // do something with the result
+                result.json().then(data => {
+                  console.log(data)
+                  var docExists = (data.result.docExists.toLowerCase() == "true")
+                  console.log(docExists)
+                  that.setState({FormInfoExists: docExists})
+                  that.setState({IdToken: idToken})
+                })
+            })
+
+        }).catch(function(error) {
+          // Handle error
+        });
+    } 
+    })
+
+  }
+
+  submitMerchantPayment(myPostBody){
+    var url = 'https://kylepence.dev:5000/merchants'
+
+    fetch(url, {
+      method:"POST",
+      body: JSON.stringify(myPostBody),
+      headers:
+          {Authorization: this.state.IdToken}
+      }
+      )
+      .then(result => {
+          console.log(result)
+          // do something with the result
+          result.json().then(data => {
+            console.log(data)
+            if (data.status=="success"){
+              this.setState({FormInfoExists: true})
+            }
+          })
+      })
+    
+  }
+
+  
 
 
 
   render() {
+    const that2 = this;
+
+    const config = {
+      apiKey: "***REMOVED***",
+      authDomain: "phonepayhack.firebaseapp.com",
+      databaseURL: "https://phonepayhack.firebaseio.com",
+      projectId: "phonepayhack",
+      storageBucket: "phonepayhack.appspot.com",
+      messagingSenderId: "32460783807",
+      appId: "1:32460783807:web:76fdb309ad2af726fac60a",
+      measurementId: "G-0C830E4Y1Q"
+    };
+
+    if (!firebase.apps.length){
+        firebase.initializeApp(config);
+    }
+    const uiConfig = {
+        signInFlow: 'popup',
+        
+        callbacks: {
+          signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+            this.setState({SignedIn: true})
+            return false;
+          },
+        
+        },
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID
+        ]
+      };
+
+      if (that2.state.SignedIn){
+        if (!that2.state.FormInfoExists){
+          
+          
+          
+          
+          return (
+            <div className="MerchantLogin">
+            <h2 class="VisaBlue">First Time Merchant Form</h2>
+            <FirstTimeMerchantForm action={this.submitMerchantPayment}></FirstTimeMerchantForm>
+            <h1>Gotta Enter Info ...  {this.state.IdToken}</h1>
+          </div> 
+          )
+        }
+
+
+        return (
+          <div className="MerchantLogin">
+            <h2 class="VisaBlue">Invoice Creation Form</h2>
+              <MerchantInvoice></MerchantInvoice>
+          </div> 
+          )
+      }
     
+
     const that = this;
     return (
         <div className="MerchantLogin">
           <h2 class="VisaBlue">Login Page</h2>
             <div>
-                <h2>[form]</h2>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth() }/>
             </div>
+            <h1>Signed In: {this.state.SignedIn.toString()}</h1>
             <Button variant="secondary" onClick={()=>that.props.setWantsToLogIn(false)} id="buttonBlue">
                 Skip logging in
             </Button>
